f2254fe51871f7a4807a835e372086da79ca9068
NaveenVr46 <94039083+NaveenVr46@users.noreply.github.com>
2022-01-23 23:06:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import PriceCart from './components/PriceCart';
+function App() {
+  return (
+    <section className="pricing py-5">
+  <div className="container">
+    <div className="row">
+     <PriceCart/>
+     
+    </div>
+  </div>
+</section>
+  );
+}
+
+export default App;
